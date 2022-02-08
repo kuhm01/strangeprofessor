1fe0866c080e8e6ea3484357afaf3c5e59f7b11a
kuhm01 <kuhm01@naver.com>
2022-02-08 14:38:25 +0900

    Update token.go

--- src/token/token.go
+++ src/token/token.go
@@ -8,8 +8,35 @@ type Token struct {
 }
 
 var keywords = map[string]TokenType{
-	"과제": 과제,
-	"공지": 공지,
+	"교수님": 교수님,
+	"주세요": 공갈,
+	"줘":   공갈,
+	"내놔":  공갈,
+
+	"A": CONSTINT,
+	"B": CONSTINT,
+	"C": CONSTINT,
+	"D": CONSTINT,
+	"F": CONSTINT,
+
+	"여석신청": 여석신청,
+
+	"전공": FLAG,
+
+	"교양":   IF,
+	"교양필수": IFFLAG,
+	"교양선택": IFFLAG,
+
+	"졸업": EXITPOINT,
+
+	"재학생": DYNAMICVAR,
+	"전입":  STACKOPER,
+	"전출":  STACKOPER,
+	"전과생": DYNAMICVAR,
+
+	"공지":   PRINTER,
+	"성적발표": PRINTER,
+	"점수발표": PRINTER,
 }
 
 func LookupIdent(ident string) TokenType {
@@ -22,6 +49,29 @@ func LookupIdent(ident string) TokenType {
 const (
 	IDENT = "IDENT"
 
-	과제 = "과제" //변수 바인딩 명령문 let
-	공지 = "공지"
+	교수님 = "교수님"
+	공갈  = "공갈"
+
+	STATICVAR  = "STATICVAR"
+	DYNAMICVAR = "DYNAMICVAR"
+
+	CONSTINT = "CONSTINT"
+	INT      = "INT"
+	STRING   = "STRING"
+
+	여석신청 = "여석신청"
+
+	SEMICOLON = "SEMICOLON"
+
+	IF     = "IF"
+	IFFLAG = "IFFLAG"
+
+	FLAG = "FLAG"
+
+	STACKOPER = "STACKOPER"
+	PRINTER   = "PRINTER"
+
+	DOTHAT = "DOTHAT"
+
+	EXITPOINT = "EXITPOINT"
 )
