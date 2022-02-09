332ee88b0d3f2793970271670ed8b1e3c7351cd8
kuhm01 <kuhm01@naver.com>
2022-02-09 22:42:33 +0900

    Separate package parse

--- src/parse/parser.go
+++ src/parse/parser.go
@@ -1,10 +1,8 @@
 package parse
 
 import (
-	"fmt"
 	"professorc/src/ast"
 	"professorc/src/token"
-	"strconv"
 )
 
 func (p *Parser) parseStatement() ast.Statement {
@@ -14,26 +12,4 @@ func (p *Parser) parseStatement() ast.Statement {
 	}
 
 	return nil
-}
-
-func (p *Parser) parseYeoseokStatement() *ast.YeoseokStatement {
-	stmt := &ast.YeoseokStatement{Token: p.curToken}
-
-	if p.peekTokenIs(token.INT) {
-		p.nextToken()
-		value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
-		if err != nil {
-			msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
-			p.errors = append(p.errors, msg)
-			return nil
-		}
-		stmt.Value = value
-		if !p.expectPeek(token.JARI) {
-			return nil
-		}
-	} else {
-		return nil
-	}
-
-	return stmt
 }
---
+++ src/parse/statementparser.go
@@ -0,0 +1,30 @@
+package parse
+
+import (
+	"fmt"
+	"professorc/src/ast"
+	"professorc/src/token"
+	"strconv"
+)
+
+func (p *Parser) parseYeoseokStatement() *ast.YeoseokStatement {
+	stmt := &ast.YeoseokStatement{Token: p.curToken}
+
+	if p.peekTokenIs(token.INT) {
+		p.nextToken()
+		value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+		if err != nil {
+			msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+			p.errors = append(p.errors, msg)
+			return nil
+		}
+		stmt.Value = value
+		if !p.expectPeek(token.JARI) {
+			return nil
+		}
+	} else {
+		return nil
+	}
+
+	return stmt
+}
